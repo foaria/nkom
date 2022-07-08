a53e4cd68d7117f9c7b83c1160bad33c1bfc2c82
幻想郷のけんけん <51988985+gensokyo-kenken@users.noreply.github.com>
2022-07-08 21:17:38 +0900

    Update MainClass.php

--- src/foaria/Nkom/MainClass.php
+++ src/foaria/Nkom/MainClass.php
@@ -1,121 +1,82 @@
 <?php
-namespace foaria\Nkom\Fetch;
+namespace foaria\Nkom;
 use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\Server;
 use pocketmine\utils\Config;
 use pocketmine\plugin\ApiVersion;
 use pocketmine\network\mcpe\protocol\ProtocolInfo;
+use foaria\Nkom\Fetch;
+require_once 'Fetch.php';
 
-class Fetch extends AsyncTask {
-  public function __construct($repos, string $name, string $query, $version, String $apiversion, $mcpe, Server $server, CommandSender $sender) {
-    $this->repos = $repos;
-    $this->name = $name;
-    $this->query = $query;
-    $this->version = $version;
-    $this->apiversion = $apiversion;
-    $this->mcpe = $mcpe;
-    $this->storeLocal('server', $server);
-    $this->storeLocal('sender', $sender);
+class MainClass extends PluginBase{
+  public function onEnable():void{
+      $this->saveDefaultConfig();
+      $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
   }
-  public function onRun() : void {
-    $this->publishProgress('{"type":"message", "message":"プラグインを探しています..."}');
-    $repos = $this->repos;
-    foreach($repos as $repo){
-        switch($repo['type']){
-          case 'dynamic':
-            $this->publishProgress('{"type":"message", "message":"' . $repo['name'] . 'から検索中..."}');
-            $params = [
-                'type' => $this->query,
-                'name' => $this->name,
-                'version' => $this->version,
-            ];
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $repo['url'] . '?' . http_build_query($params)); 
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $data =  curl_exec($ch);
-            curl_close($ch);
-            if(!$data){
-                $this->publishProgress('{"type":"message", "message":"' .$repo['name'] . 'は利用できません:'.curl_error($ch) .'"}');
-            }else if(isset(json_decode($data, true)['info']) && !isset(json_decode($data, true)['error'])){
-                $response = json_decode($data, true);
-                $this->publishProgress('{"type":"message", "message":"プラグインが見つかりました:'. $response['info']['name'] .' "}');
-                if(ApiVersion::isCompatible($this->apiversion, $response['info']['api_version'])){
-                    if(isset($response['info']['mcpe-protocol'])){
-                        $i = 0;
-                        foreach($response['info']['mcpe-protocol'] as $mcpe_proto){
-                            $i++;
-                            if($mcpe_proto == $this->mcpe){
-                                $protoc = true;
-                            }else if(count($response['info']['mcpe-protocol']) == $i){
-                                $this->publishProgress('{"type":"message", "message":"§c' .$response['info']['name']. 'は現在実行しているPMMPのプロトコルバージョンに対応していません。"}');
-                                $this->setResult('{"exit":"error"}');
-                                return;
-                            }
-                        }
-                    }
-                    if(isset($response['info']['depend'])){
-                        $this->publishProgress('{"type":"message", "message":"' .$response['info']['name']. 'の依存プラグインが見つかりました:"}');
-                        foreach($response['info']['depend'] as $depend){
-                            $this->publishProgress('{"type":"message", "message":"' .$depend. '"}');
-                        }
-                        $this->publishProgress('{"type":"message", "message":""}');
-                        $this->publishProgress('{"type":"depend", "depend":' .json_encode($response['info']['depend']). '}');
-                    }
-                    $this->publishProgress('{"type":"message", "message":"' .$response['info']['name']. 'をダウンロードしています..."}');
-                    $ch = curl_init();
-                    curl_setopt($ch, CURLOPT_URL, $response['info']['url']); 
-                    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-                    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                    $data =  curl_exec($ch);
-                    curl_close($ch);
-                    if($data){
-                        if(file_put_contents(realpath('./plugins').'/'.$response['info']['name'].".phar",$data) === false){
-                            $this->publishProgress('{"type":"message", "message":"§c' .$response['info']['name']. 'のインストールに失敗しました。"}');
-                        }else{
-                            $this->publishProgress('{"type":"message", "message":"§a' .$response['info']['name']. 'をインストールしました。"}');
-                        }
-                    }else{
-                        $this->publishProgress('{"type":"message", "message":"§c' .$response['info']['name']. 'のダウンロードに失敗しました。"}');
-                        $this->setResult('{"exit":"error"}');
-                    }
-                }else{
-                    $this->publishProgress('{"type":"message", "message":"§c' .$response['info']['name']. 'は現在実行しているPMMPと互換性がありません。"}');
-                    $this->setResult('{"exit":"error"}');
-                }
-            }else{
-                switch(json_decode($data, true)['error']){
-                  case 'version not found':
-                  $this->publishProgress('{"type":"message", "message":"§c' .$this->name. 'の指定したバージョンは存在しません。"}');
-                    $this->setResult('{"exit":"error"}');
-                }
+  public function onCommand(CommandSender $sender, Command $command, string $label, array $args):bool {
+    switch($command){
+      case 'nkom':
+        if(isset($args[0])){
+          //install start
+          if($args[0] == 'i' or $args[0] == 'install' or $args[0] == 'add'){
+            if(isset($args[1])){
+              $server = $this->getServer();
+              //install process start
+              $apiversion = $server->getApiVersion();
+              $plugin_name = $args[1];
+              $plugin_version = null;
+              if(strpos($args[1], '@') != false){
+                  $name_no_edit = explode('@', $args[1]);
+                  $plugin_name = $name_no_edit[0];
+                  $plugin_version = $name_no_edit[1];
+              }
+              $task = new Fetch($this->config->get('repositorys'), $plugin_name, 'install', $plugin_version, $apiversion, ProtocolInfo::CURRENT_PROTOCOL ,$server, $sender);
+              $server->getAsyncPool()->submitTask($task);
+              //install process end
+              return true;
+            }else {
+              $sender->sendMessage("使い方: /nkom ". $args[0] . " [プラグイン名またはダウンロードリンク]");
+              return true;
             }
-          
+          //install end
+          }else if($args[0] == 'u' or $args[0] == 'update'){
+            if(isset($args[1])){
+              //update process start
+              $sender->sendMessage('アップデートを開始します。');
+              //update process end
+              return true;
+            }else {
+              $sender->sendMessage("使い方: /nkom ". $args[0] . " [プラグイン名]");
+              return true;
+            }
+          }else if($args[0] == 'remove' or $args[0] == 'r' or $args[0] == 'm' or $args[0] == 'rm'){
+              }
+        return false;
         }
-    }
-    $this->setResult('{"type":"message", "message":"§cプラグインが見つかりませんでした"}');
-  }
-  public function onCompletion() : void {
-    $result = $this->getResult();
-    $sender = $this->fetchLocal('sender');
-  }
-  public function onProgressUpdate($log) : void {
-    $sender = $this->fetchLocal('sender');
-    $server = $this->fetchLocal('server');
-    if(json_decode($log, true)['type'] == 'message'){
-        $sender->sendMessage(json_decode($log, true)['message']);
-    }
-    if(json_decode($log, true)['type'] == 'depend'){
-        foreach(json_decode($log, true)['depend'] as $depend){
-            $repos = $this->repos;
-            $task = new Fetch($repos, $depend, 'install', null, $this->apiversion, ProtocolInfo::CURRENT_PROTOCOL ,$server, $sender);
-            $server->getAsyncPool()->submitTask($task);
+      return false;
+      //nkom end
+      case 'nkom-repo':
+        if(isset($args[0])){
+          //add start
+          if($args[0] == 'a' or $args[0] == 'add'){
+            if(isset($args[1])){
+              $sender->sendMessage('リポジトリを追加しました。');
+              return true;
+            }else {
+              $sender->sendMessage("使い方: /nkom-repo ". $args[0] . " [リポジトリのURL]");
+              return true;
+            }
+          //install end
+          }
+        return false;
         }
+      return false;
+      //nkom-repo end
     }
+  //switch end
   }
-
 }
